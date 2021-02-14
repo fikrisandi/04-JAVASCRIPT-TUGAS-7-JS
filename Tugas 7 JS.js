4055c0e2bdd6e06f260bd9c9fb2bd4a04cd2163b
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-14 20:10:32 +0700

    Tugas 7 JS

---
+++ Tugas 7 JS.js
@@ -0,0 +1,10 @@
+var Tinggi_Badan = [160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210];
+var i;
+
+for (i = 0; i < Tinggi_Badan.length; i++) {
+    console.log(Tinggi_Badan[i]);
+}
+
+for (let x of Tinggi_Badan) {
+    console.log(x);
+}
